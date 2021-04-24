2d79197b43c6e728b04b3c8200fd501aff2dd0fb
Farhan Hasin Chowdhury <mail@farhan.info>
2021-04-24 18:26:07 +0600

    simplified demos

--- node-js-demo/app.js
+++ node-js-demo/app.js
@@ -1,113 +1,12 @@
-const crypto = require('crypto');
-const express = require('express');
+const http = require('http');
 
-const app = express();
-const data = {
-    notes: [],
-};
-
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'all notes',
-        data: {
-            notes: data.notes,
-        }
-    });
-});
-
-app.post('/', (req, res) => {
-    const { title, text } = req.body;
-
-    if (!title || !text) {
-        res.status(400).json({
-            status: 'fail',
-            message: 'title and text is required'
-        });
-    } else {
-        const note = {
-            id: crypto.randomBytes(8).toString('hex'),
-            title,
-            text,
-        };
+const server = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "application/json");
     
-        data.notes.push(note);
-    
-        res.status(201).json({
-            status: 'success',
-            message: 'note created',
-            data: {
-                note,
-            }
-        });
-    }
+    res.writeHead(200);
+    res.end(`{ "status": "success", "message": "You're reading The NGINX Handbook!" }`);
 });
 
-app.get('/:id', (req, res) => {
-    const note = data.notes.find((note) => note.id === req.params.id);
-
-    if (!note) {
-        res.status(404).json({
-            status: 'fail',
-            message: 'not found'
-        });
-    } else {
-        res.status(200).json({
-            status: 'success',
-            message: 'single note',
-            data: {
-                note,
-            }
-        });
-    }
-});
-
-app.put('/:id', (req, res) => {
-    const index = data.notes.map((note) => note.id).indexOf(req.params.id);
-
-    if (index === -1) {
-        res.status(404).json({
-            status: 'fail',
-            message: 'not found'
-        });
-    } else {
-        data.notes[index].title = req.body.title ? req.body.title : data.notes[index].title,
-        data.notes[index].text = req.body.text ? req.body.text : data.notes[index].text,
-
-        res.status(200).json({
-            status: 'success',
-            message: 'note updated',
-            data: {
-                note: data.notes[index],
-            }
-        });
-    }
-});
-
-app.delete('/:id', (req, res) => {
-    const index = data.notes.map((note) => note.id).indexOf(req.params.id);
-
-    if (index === -1) {
-        res.status(404).json({
-            status: 'fail',
-            message: 'not found'
-        });
-    } else {
-        data.notes.splice(index, 1);
-
-
-        res.status(200).json({
-            status: 'success',
-            message: 'note deleted',
-            data: {
-                note: null,
-            }
-        });
-    }
-});
-
-app.listen(3000, () => {
-    console.log('listening at http://localhost:3000');
+server.listen(3000, 'localhost', () => {
+    console.log('running on http://localhost:3000/');
 });
---
+++ php-demo/index.php
@@ -0,0 +1,9 @@
+<?php
+
+header('Content-type: application/json');
+
+http_response_code(200);
+echo json_encode([
+    'status' => 'success',
+    'message' => 'You\'re reading The NGINX Handbook!'
+]);
