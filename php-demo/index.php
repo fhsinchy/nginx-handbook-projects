6a11dbe9786b3e38257e29e6e457ad41b4706863
Farhan Hasin Chowdhury <mail@farhan.info>
2021-04-24 20:56:37 +0600

    Added new lines

--- php-demo/index.php
+++ php-demo/index.php
@@ -5,5 +5,5 @@ header('Content-type: application/json');
 http_response_code(200);
 echo json_encode([
     'status' => 'success',
-    'message' => 'You\'re reading The NGINX Handbook!'
+    'message' => 'You\'re reading The NGINX Handbook!\n',
 ]);
