0ac0f5383903af8ca976f537831394ca90015323
Farhan Hasin Chowdhury <mail@farhan.info>
2021-04-25 18:17:21 +0600

    updated response string

--- load-balancer-demo/server-2.js
+++ load-balancer-demo/server-2.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "application/json");
     
     res.writeHead(200);
-    res.end("response from server - 2.\n");
+    res.end(`{ "status": "success", "message": "response from server - 2.\n" }`);
 });
 
 server.listen(3001, 'localhost', () => {
